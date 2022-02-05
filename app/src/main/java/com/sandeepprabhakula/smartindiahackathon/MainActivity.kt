0edcd829fd5757a31cc2375df7e08789788b5731
sandeep <sandeepprabhakula10@gmail.com>
2022-02-05 12:03:37 +0530

    added internet connectivity check

--- app/src/main/java/com/sandeepprabhakula/smartindiahackathon/MainActivity.kt
+++ app/src/main/java/com/sandeepprabhakula/smartindiahackathon/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.sandeepprabhakula.smartindiahackathon
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.sandeepprabhakula.smartindiahackathon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val auth = FirebaseAuth.getInstance()
         val user = auth.currentUser
-        if(user!=null){
-            val intent = Intent(this,WorkingFragmentsHolder::class.java)
-            startActivity(intent)
-            finish()
+        if (!checkForInternet()) {
+            val snackBar = Snackbar.make(
+                binding.fragmentContainerView, "No Internet Connection",
+                Snackbar.LENGTH_LONG
+            ).setAction("Action", null)
+            snackBar.setActionTextColor(Color.WHITE)
+            val snackBarView = snackBar.view
+            snackBarView.setBackgroundColor(Color.DKGRAY)
+            val textView = snackBarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
+            textView.setTextColor(Color.WHITE)
+            snackBar.setAction("CONNECT") {
+                val intent = Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS)
+                startActivity(intent)
+            }
+            snackBar.show()
+        } else {
+            if (user != null) {
+                val intent = Intent(this, WorkingFragmentsHolder::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
+    }
+
+    private fun checkForInternet(): Boolean {
+        val connectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkForInternet()
     }
 }
