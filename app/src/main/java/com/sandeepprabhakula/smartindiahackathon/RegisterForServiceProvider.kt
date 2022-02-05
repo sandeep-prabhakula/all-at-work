4d8a42e2422bd7d7288402be3b25961979d69a82
sandeep <sandeepprabhakula10@gmail.com>
2022-02-05 11:17:50 +0530

    added cities dropdown

--- app/src/main/java/com/sandeepprabhakula/smartindiahackathon/RegisterForServiceProvider.kt
+++ app/src/main/java/com/sandeepprabhakula/smartindiahackathon/RegisterForServiceProvider.kt
@@ -5,9 +5,7 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.sandeepprabhakula.smartindiahackathon.daos.WorkersDao
 import com.sandeepprabhakula.smartindiahackathon.models.Worker
@@ -28,6 +26,30 @@ class RegisterForServiceProvider : Fragment() {
         val newWorkerLocation: EditText = view.findViewById(R.id.newWorkerLocation)
         val registerWorker: Button = view.findViewById(R.id.registerWorker)
         val newWorkerPINCode: EditText = view.findViewById(R.id.newWorkerPINCode)
+        val spinner: Spinner = view.findViewById(R.id.locationSpinner)
+        val cities = resources.getStringArray(R.array.Cities)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item, cities
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    newWorkerLocation.setText(cities[position])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
         registerWorker.setOnClickListener {
             val name = newWorkerName.text.toString()
             val mobile = newWorkerMobile.text.toString()
@@ -38,11 +60,21 @@ class RegisterForServiceProvider : Fragment() {
             if (!TextUtils.isEmpty(name) &&
                 !TextUtils.isEmpty(age) &&
                 !TextUtils.isEmpty(mobile) && mobile.length == 10 &&
-                !TextUtils.isEmpty(location) &&
+                !TextUtils.isEmpty(location) && location != "Select a city" &&
                 !TextUtils.isEmpty(skills) &&
                 !TextUtils.isEmpty(pinCode)
             ) {
-                workersDao.addWorker(Worker(name, age, mobile, skills, 0, location.lowercase(),pinCode))
+                workersDao.addWorker(
+                    Worker(
+                        name,
+                        age,
+                        mobile,
+                        skills,
+                        0,
+                        location.lowercase(),
+                        pinCode
+                    )
+                )
                 Toast.makeText(activity, "Registration Successful ", Toast.LENGTH_SHORT)
                     .show()
                 newWorkerAge.setText("")
